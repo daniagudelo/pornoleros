5d392d46518a9e1034bb45772cc9f000f64f2b6f
daniagudelo <daniagudelo@gmail.com>
2016-12-20 21:28:08 +0000

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 105 },
+  uniqueness: { case_sensitive: false },
+  format: { with: VALID_EMAIL_REGEX }
+
+  validates_presence_of :password
+  
+  
+end
