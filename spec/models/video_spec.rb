2b9486966003596260d21585a7dd898e9d2ce523
daniagudelo <daniagudelo@gmail.com>
2016-12-21 21:03:18 +0000

    imported everything from test app

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
+    it "is not be valid without a pornstar" do
+        video = Video.new(pornstars: nil)
+        expect(video).to_not be_valid
+    end
   pending "add some examples to (or delete) #{__FILE__}"
 end
